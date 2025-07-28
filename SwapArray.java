9ec75d14783e60addfb8fc8071cbfbef0302ac3e
SHIVEN SINGH <sshiven05@gmail.com>
2025-07-28 21:31:50 +0530

    Create SwapArray.java

---
+++ SwapArray.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        swap(arr, 0, 4);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
