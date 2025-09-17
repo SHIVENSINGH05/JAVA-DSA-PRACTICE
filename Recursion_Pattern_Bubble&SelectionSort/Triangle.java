b41ae373a531662db2317725e171c1a596be4010
SHIVEN SINGH <sshiven05@gmail.com>
2025-09-17 20:33:22 +0530

    Add files via upload

---
+++ Recursion_Pattern_Bubble&SelectionSort/Triangle.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+
+public class Triangle {
+    public static void main(String[] args) {
+        // triangle(5, 0);
+        int[] arr = {4, 3, 2, 1};
+        bubble(arr, arr.length - 1, 0);
+        System.err.println(Arrays.toString(arr));
+
+    }
+
+    // static void triangle(int r, int c) {
+    //     if (r == 0) {
+    //         return;
+    //     }
+    //     if (c < r) {
+    //         System.out.print("*");
+    //         triangle(r, c + 1);
+    //     } else {
+    //         System.out.println();
+    //         triangle(r - 1, 0);
+    //     }
+    // }
+
+    static void bubble(int[] arr, int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            
+            if (arr[c] > arr[c+1]) {
+                //swap
+                int temp = arr[c];
+                arr[c] = arr[c+1];
+                arr[c+1] = temp;
+
+            }
+            bubble(arr, r, c+1);
+        } else {
+            bubble(arr, r-1, 0);
+        }
+    }
+
+    static void selection(int[] arr, int r, int c) {
+        if (r == 0) {
+            return;
+        }
+        if (c < r) {
+            if (arr[c] > arr[c + 1]) {
+                selection(arr, r, c+1, c);
+            } else {
+                selection(arr, r, c+1, max);
+            }
+        } else {
+            int temp = arr[max];
+            arr[max] = arr[r-1];
+            arr[r-1] = temp;
+            selection(arr,r - 1, 0, 0);
+        }
+    }
+}
+        
